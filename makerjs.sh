4d11aee843d03c8418fd4bb7cbd3ff1065eedb82
timothy adigun <adigun.timothy@gmail.com>
2026-03-09 19:17:20 +0000

    make the helper function

--- makerjs.sh
+++ makerjs.sh
@@ -3,3 +3,13 @@
 # date:        09/03/26
 # description: Create react components, html templates, and css stylesheets
 #
+
+function help() {
+    echo "Usage: makerjs.sh <options> <filename>"
+    echo "Options:"
+    echo "  -h, --help      Display this help message"
+    echo "  -c, --component <component-name>  Create a new react component"
+    echo "  -t, --template <template-name>    Create a new html template"
+    echo "  -s, --style <style-name>          Create a new css stylesheet"
+    exit 0
+}
