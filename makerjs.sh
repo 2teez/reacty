b0f34e7331aec4c987dbdf808c4bee4a7c8d5b0b
timothy adigun <adigun.timothy@gmail.com>
2026-03-10 11:42:57 +0000

    adding deleting a project in rjs

--- makerjs.sh
+++ makerjs.sh
@@ -8,6 +8,7 @@ function help() {
     echo "Usage: makerjs.sh <options> <filename>"
     echo "Options:"
     echo "  -h, --help      Display this help message"
+    echo "  -p, --project   Create a new react project"
     echo "  -c, --component <component-name>  Create a new react component"
     echo "  -t, --template <template-name>    Create a new html template"
     echo "  -s, --style <style-name>          Create a new css stylesheet"
@@ -23,18 +24,45 @@ for arg in "$@"; do
     case "${arg}" in
         --help) set -- "$@" "-h" ;;
         --component) set -- "$@" "-c" ;;
+        --delete) set -- "$@" "-d" ;;
+        --project) set -- "$@" "-p" ;;
         --template) set -- "$@" "-t" ;;
         --style) set -- "$@" "-s" ;;
         *) set -- "$@" "${arg}" ;;
     esac
 done
 
-options="hc:t:s:"
+options="hc:d:t:s:p:"
 while getopts "${options}" opt; do
     case "${opt}" in
         h) help ;;
         c) echo "Creating component: ${OPTARG}" ;;
+        d) echo "Deleting component: ${OPTARG}"
+        if ! [[ -e "${OPTARG}" ]]; then
+            echo "${OPTARG} not found"
+            exit 0
+        fi
+           while true; do
+               read -p "Are you sure you want to delete ${OPTARG}? (y/n) " yn
+               case $yn in
+                   [Yy]* ) break;;
+                   [Nn]* ) exit 0;;
+                   * ) echo "Please answer yes or no.";;
+               esac
+           done
+           rm -rf ${OPTARG}
+           exit 0
+           ;;
         t) echo "Creating template: ${OPTARG}" ;;
         s) echo "Creating style: ${OPTARG}" ;;
+        p) echo "Creating project: ${OPTARG}"
+           npm init react-app ${OPTARG} --template typescript
+           cd ${OPTARG}
+           npm start
+           exit 0
+            ;;
+        *)
+            echo "Invalid option: ${OPTARG}"
+            help ;;
     esac
 done
