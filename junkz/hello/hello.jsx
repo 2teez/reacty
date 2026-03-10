8a5b1e5d0bdb6659417731b733f385f22e59d788
timothy adigun <adigun.timothy@gmail.com>
2026-03-10 17:43:09 +0000

    added other function label

--- junkz/hello/hello.jsx
+++
@@ -1,4 +0,0 @@
- const Greet = ({ name }) => <hl>Hello {name}!</hl>;
-    const rootElement = document.getElementById('root');
-    const root = ReactDOM.createRoot(rootElement);
-    root.render(<Greet name="Reader" />);
