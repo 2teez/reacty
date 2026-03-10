7bffda09e58c31c18d2e87c95b114a08a47f5d55
timothy adigun <adigun.timothy@gmail.com>
2026-03-10 18:39:31 +0000

    booklist examples

---
+++ junkz/hello/src/App.jsx
@@ -0,0 +1,12 @@
+import BookList from "./BookList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Books Management</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
